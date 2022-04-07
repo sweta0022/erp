5f46236bd9dfea896de3987c86d22eeb1d566529
sweta0022 <swetagupta0022@gmail.com>
2022-04-07 18:35:50 +0530

    debug

--- app/Models/category.php
+++ app/Models/category.php
@@ -6,7 +6,7 @@ use App\Models\ItemMaster;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class category extends Model
+class Category extends Model
 {
     protected $fillable = [
         'name',
